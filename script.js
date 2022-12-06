c6b54144a62b88839a8ac362e8b7f2ae90b6770c
caominhnhut22 <caonhut2004@gmail.com>
2022-12-06 20:38:55 +0700

    update source code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('.img-btn').addEventListener('click', function()
+	document.querySelector('.img-btn').addEventListener('click', function()
 	{
 		document.querySelector('.cont').classList.toggle('s-signup')
 	}
